e4ea32a7530f3ebc5dbaf35bc3f13c8aec6e41d2
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-26 18:51:10 +0200

    Inserting and Deleting Data using Spring JDBC

---
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/Course.java
@@ -0,0 +1,41 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course;
+
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+
+    public Course(){
+
+    }
+
+    //Constructor
+    public Course(long id, String name, String author) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+    //Getter
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Course[" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ']';
+    }
+}
--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,16 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, " LEARN AZURE Now!", "in28minutes"));
+        repository.insert(new Course(2, " LEARN AWS Now!", "in28minutes"));
+        repository.insert(new Course(3, " LEARN Docker Now!", "in28minutes"));
+        repository.insert(new Course(4, " LEARN React Now!", "in28minutes"));
+        repository.insert(new Course(5, " LEARN Java Now!", "in28minutes"));
+
+        repository.deleteById(1);
     }
+
+
+
+
 }
